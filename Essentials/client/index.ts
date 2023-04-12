be1f529684085f606ba277714f931f95367a7527
appledog2k <appledog2k@gmail.com>
2023-04-12 09:55:59 +0700

    Transport Protocol

--- Essentials/client/index.ts
+++ Essentials/client/index.ts
@@ -1,20 +1,31 @@
 import * as signalR from "@microsoft/signalr";
-import { CustomLogger } from "./customLogger";
+import * as customLogger from "./CustomLogger";
 
+// WebSocket = undefined; // disable websockets
 var counter = document.getElementById("viewCounter");
+
 // Create connection to the server
 let connection = new signalR.HubConnectionBuilder()
-  .configureLogging(new CustomLogger())
-  .withUrl("/hub/view")
+  // .configureLogging(new customLogger.CustomLogger())
+  .withUrl("/hub/view", {
+    transport:
+      signalR.HttpTransportType.WebSockets |
+      signalR.HttpTransportType.ServerSentEvents |
+      signalR.HttpTransportType.LongPolling,
+    // timeout: 1,
+  })
   .build();
+
 // on view update message from client
 connection.on("viewCountUpdate", (value: number) => {
   counter.innerText = value.toString();
 });
+
 // notify server we are watching the view
 function notify() {
   connection.send("notifyWatching");
 }
+
 // start connection
 function startSuccess() {
   console.log("Connected");
@@ -23,4 +34,5 @@ function startSuccess() {
 function startFail() {
   console.log("Connection failed");
 }
+
 connection.start().then(startSuccess, startFail);
